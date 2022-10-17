5f0d0458c8a34503bffe0cafc62399882692cfcf
divyagaikwad27 <111782150+divyagaikwad27@users.noreply.github.com>
2022-10-17 14:29:34 +0530

    Add files via upload

---
+++ MiniProject_ATMdbSystem/MidiChannel.java
@@ -0,0 +1,160 @@
+
+public class MidiChannel implements javax.sound.midi.MidiChannel {
+
+	@Override
+	public void noteOn(int noteNumber, int velocity) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void noteOff(int noteNumber, int velocity) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void noteOff(int noteNumber) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setPolyPressure(int noteNumber, int pressure) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getPolyPressure(int noteNumber) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setChannelPressure(int pressure) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getChannelPressure() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void controlChange(int controller, int value) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getController(int controller) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void programChange(int program) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void programChange(int bank, int program) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getProgram() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setPitchBend(int bend) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getPitchBend() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void resetAllControllers() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void allNotesOff() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void allSoundOff() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean localControl(boolean on) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setMono(boolean on) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean getMono() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setOmni(boolean on) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean getOmni() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setMute(boolean mute) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean getMute() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setSolo(boolean soloState) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean getSolo() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
